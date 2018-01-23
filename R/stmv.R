76ec0dd917339ce0b6446edfb9be69aa6cede833
Jae S Choi <choijaeseok@gmail.com>
2018-01-23 09:33:57 -0400

    missing flags

--- R/stmv.R
+++ R/stmv.R
@@ -470,6 +470,10 @@ stmv = function( p, runmode, DATA=NULL, storage.backend="bigmemory.ram",  debug_
   if ( "restart" %in% runmode ) {
     stmv_db( p=p, DS="load_saved_state" ) # load saved state back into memory .. otherwise use what is in memory
     toredo = stmv_db( p=p, DS="flag.incomplete.predictions" )
+    if ( !is.null(toredo) && length(toredo) > 0) {
+      Sflag = stmv_attach( p$storage.backend, p$ptr$Sflag )
+      Sflag[toredo]=0L
+    }
   } 
   
 
