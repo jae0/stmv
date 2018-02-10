a2e50c1d0a6060a6269887adcd807e79410e0c48
Jae S Choi <choijaeseok@gmail.com>
2018-02-09 22:22:21 -0400

    misc

--- R/stmv_interpolate.R
+++ R/stmv_interpolate.R
@@ -89,7 +89,8 @@ stmv_interpolate = function( ip=NULL, p, debug=FALSE, stime=Sys.time(), ... ) {
   for ( iip in ip ) {
 
     localcount = localcount + 1
-    if (( localcount %% sample.int(11, 1) == 0))  currentstatus = stmv_logfile(p=p, stime=stime, local.n.complete=p$local.n.complete)
+    if (( localcount %% sample.int(11, 1) == 0))  currentstatus = stmv_logfile(p=p, stime=stime)
+    
 
     Si = p$runs[ iip, "locs" ]
     print( paste(iip, Si ) )
