2829cf06fe35da66329aab2c424f93b4dc49a8eb
Jae S Choi <choijaeseok@gmail.com>
2018-05-24 20:47:57 -0300

    debug

--- R/stmv__fft.R
+++ R/stmv__fft.R
@@ -1,12 +1,14 @@
 
 stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist=FALSE, ... ) {
 
-  #\\ this is the core engine of stmv .. localised space (no-time) modelling interpolation 
-  #\\ note: time is not being modelled and treated independently 
+  #\\ this is the core engine of stmv .. localised space (no-time) modelling interpolation
+  #\\ note: time is not being modelled and treated independently
   #\\      .. you had better have enough data in each time slice
   #\\ first a low-pass filter as defined by p$stmv_lowpass_nu, p$stmv_lowpass_phi, then a simple covariance filter determined by nu,phi
-  
+
   if (variablelist)  return( c() )
+
+browser()
 
   sdTotal=sd(dat[,p$variable$Y], na.rm=T)
 
@@ -15,7 +17,7 @@ stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist
 
   pa_r = range(pa[,p$variables$LOCS[1]])
   pa_c = range(pa[,p$variables$LOCS[2]])
-  
+
   nr = round( diff(x_r)/p$pres ) + 1
   nc = round( diff(x_c)/p$pres ) + 1
 
@@ -25,7 +27,7 @@ stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist
   x_locs = expand.grid( x_plons, x_plats ) # final output grid
   attr( x_locs , "out.attrs") = NULL
   names( x_locs ) = p$variables$LOCS
-  
+
   dat$mean = NA
   pa$mean = NA
   pa$sd = sdTotal  # this is ignored with fft
@@ -38,10 +40,10 @@ stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist
   # constainer for spatial filters
   dgrid = make.surface.grid(list((1:nr2) * dx, (1:nc2) * dy))
   center = matrix(c((dx * nr2)/2, (dy * nc2)/2), nrow = 1, ncol = 2)
-  
+
   mC = matrix(0, nrow = nr2, ncol = nc2)
   mC[nr, nc] = 1
- 
+
   if (!exists("stmv_fft_filter",p) ) p$stmv_fft_filter="lowpass" # default in case of no specification
 
   if ( p$stmv_fft_filter == "lowpass") {
@@ -68,40 +70,40 @@ stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist
   sp.covar = sp.covar2 = sp.covar.surf = sp.covar.surf2 = dgrid = center = mC = NULL
   gc()
 
-  xi =1:nrow(dat) 
+  xi =1:nrow(dat)
   pa_i = 1:nrow(pa)
   origin=c(x_r[1], x_c[1])
   res=c(p$pres, p$pres)
 
   for ( ti in 1:p$nt ) {
 
-    if ( exists("TIME", p$variables)) xi = which( dat[, p$variables$TIME]==p$prediction.ts[ti] ) 
-    
+    if ( exists("TIME", p$variables)) xi = which( dat[, p$variables$TIME]==p$prediction.ts[ti] )
+
     # map of row, col indices of input data in the new (output) coordinate system
-    
+
     x_id = array_map( "xy->2", coords=dat[xi,p$variables$LOCS], origin=origin, res=res )
-    
+
     u = as.image( dat[xi,p$variables$Y], ind=as.matrix( x_id), na.rm=TRUE, nx=nr, ny=nc )
-    
+
     mN = matrix(0, nrow = nr2, ncol = nc2)
     mN[1:nr,1:nc] = u$weights
     mN[!is.finite(mN)] = 0
-    
+
     mY = matrix(0, nrow = nr2, ncol = nc2)
     mY[1:nr,1:nc] = u$z
     mY[!is.finite(mY)] = 0
-  
+
     u = NULL
 
     # low pass filter based upon a global nu,phi .. remove high freq variation
     fN = Re(fft(fft(mN) * sp.covar.kernel, inverse = TRUE))[1:nr,1:nc]
     fY = Re(fft(fft(mY) * sp.covar.kernel, inverse = TRUE))[1:nr,1:nc]
-    
+
     mY = mN = NULL
 
     Z = fY/fN
     fY = fN = NULL
-    
+
     if (exists("TIME", p$variables) ) pa_i =  which( pa[, p$variables$TIME]==p$prediction.ts[ti])
 
     Z_i = array_map( "xy->2", coords=pa[pa_i,p$variables$LOCS], origin=origin, res=res )
@@ -117,14 +119,13 @@ stmv__fft = function( p=NULL, dat=NULL, pa=NULL, nu=NULL, phi=NULL, variablelist
 
     # pa$sd[pa_i] = NA  ## fix as NA
     Z = NULL
-    
+
   }
 
 
   stmv_stats = list( sdTotal=sdTotal, rsquared=NA, ndata=nrow(dat) ) # must be same order as p$statsvars
-  
+
   # lattice::levelplot( mean ~ plon + plat, data=pa, col.regions=heat.colors(100), scale=list(draw=FALSE) , aspect="iso" )
 
-  return( list( predictions=pa, stmv_stats=stmv_stats ) )  
+  return( list( predictions=pa, stmv_stats=stmv_stats ) )
 }
-
