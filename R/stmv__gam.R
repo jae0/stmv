a8c1a7e2e72c35e719ade82183db92834b62615d
Jae S Choi <choijaeseok@gmail.com>
2018-05-25 12:47:36 -0300

    comment

--- R/stmv__gam.R
+++ R/stmv__gam.R
@@ -14,7 +14,7 @@ stmv__gam = function( p=NULL, dat=NULL, pa=NULL, variablelist=FALSE, ... ) {
       hmod = try( bam( p$stmv_local_modelformula, data=dat, na.action="na.omit", weights=weights ) )
     }
   } else {
-      hmod = try( gam( p$stmv_local_modelformula, data=dat, na.action="na.omit", optimizer=c("outer", "bfgs")  ) )
+      hmod = try( gam( p$stmv_local_modelformula, data=dat, na.action="na.omit", optimizer=c("outer", "bfgs")  ) )  # gam has become very slow as an option...
   }
 
 
