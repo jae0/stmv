1a01254cb8b6bc85cfaff267c0da7760a00b04f7
Jae S Choi <choi.jae.seok@gmail.com>
2019-02-20 08:25:14 -0400

    remove sampling param

--- R/stmv_parameters.R
+++ R/stmv_parameters.R
@@ -51,12 +51,8 @@ stmv_parameters = function( p=NULL, ... ) {
   if (!exists( "depth.filter", p)) p$depth.filter = FALSE # if !FALSE .. depth is given as m so, choose andy stats locations with elevation > 1 m as being on land
   if (!exists( "stmv_kernelmethods_use_all_data", p)) p$stmv_kernelmethods_use_all_data =TRUE ## speed and RAM usage improvement is minimal (if any) when off, leave on or remove option and fix as on
 
-  if ( !exists("sampling", p))  p$sampling = c( 1, 1.25 ) # fractions of distance scale to try iteratively increasing distance and n.min
-
   # downsampling (thinning) of data via a simple discretization and uniform weight of data counts in each block .. 1== use current discretization (pres, pres, tres)
   if ( !exists("downsampling_multiplier", p))  p$downsampling_multiplier = 1
-
-
 
   if (!exists("stmv_lowpass_phi", p)) p$stmv_lowpass_phi = p$pres*2 # FFT based method when operating gloablly
   if (!exists("stmv_lowpass_nu", p)) p$stmv_lowpass_nu = 0.5 # this is exponential covar
