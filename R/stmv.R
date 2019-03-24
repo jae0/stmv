bc4ea3cfa4ff69e7485c65fa3b6d3aabd85dca2d
Jae S Choi <choi.jae.seok@gmail.com>
2019-03-24 20:24:44 -0300

    revert

--- R/stmv.R
+++ R/stmv.R
@@ -1,7 +1,7 @@
 
 
 stmv = function( p, runmode="interpolate", DATA=NULL,
-  extrapolate_predictions=FALSE, use_saved_state=NULL, save_completed_data=TRUE, force_complete_solution=TRUE, nlogs=200,
+  use_saved_state=NULL, save_completed_data=TRUE, force_complete_solution=TRUE, nlogs=200,
   debug_plot_variable_index=1, debug_data_source="saved.state", debug_plot_log=FALSE, robustify_quantiles=c(0.0005, 0.9995) ) {
 
   if (0) {
@@ -13,7 +13,7 @@ stmv = function( p, runmode="interpolate", DATA=NULL,
     save_completed_data=TRUE  # export out of stmv system for use outside (e.g., by aegis)
     debug_plot_variable_index=1
     extrapolate_predictions=FALSE
-
+    robustify_quantiles=c(0.0005, 0.9995)
     # runmode=c("interpolate", "globalmodel")
     # runmode=c("interpolate")
   }
@@ -86,16 +86,16 @@ stmv = function( p, runmode="interpolate", DATA=NULL,
 
   stmv_db( p=p, DS="cleanup" )
 
-  if (!extrapolate_predictions) {
-    # force output covars to be in the same range as input data .. no extrapolation
-    for ( vn in p$variables$COV ){
-      vnrange = range( DATA$input[,vn], na.rm=TRUE )
-      toolow = which( DATA$output$COV[[vn]] < vnrange[1] )
-      if (length(toolow) > 1) DATA$output$COV[[vn]][toolow] = vnrange[1]
-      toohigh = which( DATA$output$COV[[vn]] > vnrange[1] )
-      if (length(toohigh) > 1) DATA$output$COV[[vn]][toohigh] = vnrange[2]
-    } 
-  }
+  # if (!extrapolate_predictions) {
+  #   # force output covars to be in the same range as input data .. no extrapolation
+  #   for ( vn in p$variables$COV ){
+  #     vnrange = range( DATA$input[,vn], na.rm=TRUE )
+  #     toolow = which( DATA$output$COV[[vn]] < vnrange[1] )
+  #     if (length(toolow) > 1) DATA$output$COV[[vn]][toolow] = vnrange[1]
+  #     toohigh = which( DATA$output$COV[[vn]] > vnrange[1] )
+  #     if (length(toohigh) > 1) DATA$output$COV[[vn]][toohigh] = vnrange[2]
+  #   } 
+  # }
 
   if (exists("stmv_Y_transform", p)) {
     DATA$input[, p$variables$Y ] = p$stmv_Y_transform$transf(DATA$input[, p$variables$Y ] )
