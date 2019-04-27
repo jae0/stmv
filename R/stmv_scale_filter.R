154e436288a496f69e704832bcc2e61fe61d8211
Jae S Choi <choi.jae.seok@gmail.com>
2019-04-27 10:49:27 -0300

    logic nu

--- R/stmv_scale_filter.R
+++ R/stmv_scale_filter.R
@@ -68,13 +68,14 @@ stmv_scale_filter = function( p, Si ) {
   if (nu_redo) {
     vg$flag = "variogram_failure"
     nu_median =  median( S[ii, match("nu", p$statsvars)], na.rm=TRUE )
-    if (nu_median < 0.25 | nu_median > 4 )  {
-      nu_median = 0.5
-    }
-    if (is.finite(nu_median)) {
-      vg$nu = nu_median
-    } else {
+    if (!is.finite(nu_median)) {
       vg$nu = 0.5
+    } else {
+      if (nu_median < 0.25 | nu_median > 4 )  {
+        vg$nu = 0.5
+      } else {
+        vg$nu = nu_median
+      }
     }
   }
 
