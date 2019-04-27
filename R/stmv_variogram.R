2845c94058aaf2d3049b41e01ddde0421aba3e73
Jae S Choi <choi.jae.seok@gmail.com>
2019-04-27 08:09:57 -0300

    vario

--- R/stmv_variogram.R
+++ R/stmv_variogram.R
@@ -702,6 +702,19 @@ stmv_variogram = function( xy=NULL, z=NULL, ti=NULL,
       fix.kappa=FALSE, fix.nugget=FALSE, max.dist=out$distance_cutoff/out$stmv_internal_scale, weights="cressie" ) )
       # kappa is the smoothness parameter , also called "nu" by others incl. RF
     gc()
+
+    retry=FALSE
+    if  (inherits(vMod, "try-error") ) retry=TRUE
+    if (vMod$kappa < 0.1 | vMod$kappa > 5) retry =TRUE
+
+    if (retry) {
+      vMod = try( variofit( vEm, nugget=0.5*out$varZ, kappa=0.5, cov.model="matern",
+        ini.cov.pars=c(0.5*out$varZ, 1 ),  limits = pars.limits( phi=c(0.1, 3), kappa=c(0.1, 5), sigmasq=c(0, out$varZ*1.25) ),
+        fix.kappa=TRUE, fix.nugget=FALSE, max.dist=out$distance_cutoff/out$stmv_internal_scale, weights="cressie" ) )
+        # kappa is the smoothness parameter , also called "nu" by others incl. RF
+      gc()
+    }
+
     if  (inherits(vMod, "try-error") )  return(NULL)
     scale = matern_phi2phi( mRange=vMod$cov.pars[2], mSmooth=vMod$kappa,
       parameterization_input="geoR", parameterization_output="stmv" ) * out$stmv_internal_scale
