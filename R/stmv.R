4a9515cd49b4a67b29dae76e11d0facc81b4c721
Jae S Choi <choi.jae.seok@gmail.com>
2019-07-09 07:59:56 -0300

    error 4

--- R/stmv.R
+++ R/stmv.R
@@ -829,6 +829,7 @@ stmv = function( p, runmode=c( "globalmodel", "scale", "interpolate", "interpola
       p0$local_interpolation_correlation = p0$stmv_interpolation_correlation[j]
       currentstatus = stmv_statistics_status( p=p0, reset="incomplete" )
       if ( length(currentstatus$todo) < length(p0$clusters)) break()
+      if (j==2) browser()
       parallel_run( stmv_interpolate, p=p0, runindex=list( locs=sample( currentstatus$todo ))  )# as ram reqeuirements increase drop cpus )
       stmv_db(p=p0, DS="save_current_state", runmode=interp_runmode)
       message( paste( "Time used for <interpolate", j, ">: ", format(difftime(  Sys.time(), p0$time_start_runmode )), "\n" ) )
