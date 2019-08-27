510c299e2707e0941a026d87a769556c8a276cae
Jae S Choi <choi.jae.seok@gmail.com>
2019-08-27 15:07:36 -0300

    typo

--- R/stmv__twostep.R
+++ R/stmv__twostep.R
@@ -88,7 +88,7 @@ stmv__twostep = function( p, dat, pa, nu=NULL, phi=NULL, varObs=varObs, varSpati
 #  if (ss$r.sq < p$stmv_rsquared_threshold ) return(NULL)  # smooth/flat surfaces are ok ..
   # temporal r-squared test
   if (exists("stmv_rsquared_threshold", p)) {
-    if ( exists("stmv_stats", ts_preds) {
+    if ( exists("stmv_stats", ts_preds)) {
       if ( exists("rsquared", ts_preds$stmv_stats) ) {
         # ts_preds_rsquared = ts_preds$stmv_stats$rsquared  # store for now until return call
         if (!is.finite(ts_preds$stmv_stats$rsquared) ) return(NULL)
