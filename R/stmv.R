7433a0dd9818875325045b5c10b48ca33489f5e8
Jae S Choi <choi.jae.seok@gmail.com>
2019-09-14 19:50:43 -0300

    typo

--- R/stmv.R
+++ R/stmv.R
@@ -606,7 +606,7 @@ stmv = function( p, runmode=NULL, DATA=NULL, nlogs=100, niter=1,
         p$runmode = paste("Interpolate_correlationbasis_", p$local_interpolation_correlation, sep="")
         message( "\n||| Entering <", p$runmode, " > : ", format(Sys.time()) )
         p$clusters = p$stmv_runmode[["interpolate"]] # as ram reqeuirements increase drop cpus
-        invisible( parallel_run( stmv_interpolate, p=p, runindex=list( locs=sample( currentstatus$todo ))  )
+        invisible( parallel_run( stmv_interpolate, p=p, runindex=list( locs=sample( currentstatus$todo )) ) )
         stmv_db(p=p, DS="save_current_state", runmode=p$runmode, datasubset="predictions") )
         invisible( stmv_statistics_status( p=p ) ) # quick update before logging
         slog = stmv_logfile(p=p, flag= paste("Interpolation phase", p$runmode, "completed ...") ) # final update before continuing
