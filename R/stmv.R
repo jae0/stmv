f02454bf469cafc4dec1748f5057578104c8a759
Jae S Choi <choi.jae.seok@gmail.com>
2019-09-15 21:56:25 -0300

    reparam

--- R/stmv.R
+++ R/stmv.R
@@ -639,22 +639,26 @@ stmv = function( p, runmode=NULL, DATA=NULL, nlogs=100, niter=1,
       p$time_start_runmode = Sys.time()
       p$stmv_interpolation_basis = "distance"
       p0 = p
-      for ( j in 1:length(p$stmv_distance_scale) ) {
-        p = p0 #reset
-        p$stmv_interpolation_basis_distance = p$stmv_distance_scale[j]
-        p$runmode = paste("interpolate_distance_basis_", p$stmv_interpolation_basis_distance, sep="")
-        p$clusters = p$stmv_runmode[["interpolate_distance_basis"]][[j]] # as ram reqeuirements increase drop cpus
-        message( "\n||| Entering <", p$runmode, " > : ", format(Sys.time()) )
-        currentstatus = stmv_statistics_status( p=p, reset=c( "incomplete" ) ) # flags/filter stats locations base dupon prediction covariates. .. speed up and reduce storage
-        if ( currentstatus$n.todo == 0 ) break()
-        if ( currentstatus$n.todo < (2*length(p$clusters)) ) p$clusters = p$clusters[1] # drop to serial mode
-        invisible( parallel_run( stmv_interpolate, p=p, runindex=list( locs=sample( currentstatus$todo ) )  ) )
-        stmv_db(p=p, DS="save_current_state", runmode=p$runmode, datasubset="predictions")
-        invisible( stmv_statistics_status( p=p ) ) # quick update before logging
-        slog = stmv_logfile(p=p, flag= paste("Interpolation distance basis phase", p$runmode, "completed ...") ) # final update before continuing
+      dists = p$stmv_distance_scale
+      dists = dists[ which( dists >= min( p$stmv_distance_prediction_range ) & dists <= max( p$stmv_distance_prediction_range ) )]
+      if (length(dists) > 0 ) {
+        for ( j in 1:length(dists) ) {
+          p = p0 #reset
+          p$stmv_interpolation_basis_distance = dists[j]
+          p$runmode = paste("interpolate_distance_basis_", p$stmv_interpolation_basis_distance, sep="")
+          p$clusters = p$stmv_runmode[["interpolate_distance_basis"]][[j]] # as ram reqeuirements increase drop cpus
+          message( "\n||| Entering <", p$runmode, " > : ", format(Sys.time()) )
+          currentstatus = stmv_statistics_status( p=p, reset=c( "incomplete" ) ) # flags/filter stats locations base dupon prediction covariates. .. speed up and reduce storage
+          if ( currentstatus$n.todo == 0 ) break()
+          if ( currentstatus$n.todo < (2*length(p$clusters)) ) p$clusters = p$clusters[1] # drop to serial mode
+          invisible( parallel_run( stmv_interpolate, p=p, runindex=list( locs=sample( currentstatus$todo ) )  ) )
+          stmv_db(p=p, DS="save_current_state", runmode=p$runmode, datasubset="predictions")
+          invisible( stmv_statistics_status( p=p ) ) # quick update before logging
+          slog = stmv_logfile(p=p, flag= paste("Interpolation distance basis phase", p$runmode, "completed ...") ) # final update before continuing
+        }
+        message( paste( "Time used for <interpolations", ">: ", format(difftime(  Sys.time(), p$time_start_runmode )), "\n" ) )
+        p = p0
       }
-      message( paste( "Time used for <interpolations", ">: ", format(difftime(  Sys.time(), p$time_start_runmode )), "\n" ) )
-      p = p0
     }
 
     if(0) {
