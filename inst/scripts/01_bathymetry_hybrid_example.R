db2b157ae37b93c9284b63e6621417991b93951a
Jae S Choi <choi.jae.seok@gmail.com>
2020-12-04 05:01:35 -0400

    clean up

--- inst/scripts/01_bathymetry_hybrid_example.R
+++ inst/scripts/01_bathymetry_hybrid_example.R
@@ -1,13 +1,9 @@
 
-# Bathymetry
+#########################################
+### example 2 -- hybrid stmv/car model:
 
+## :: main difference is:  project_class="hybrid"  (vs "stmv" above)
 
-# Spatial interpolation using stmv
-
-# FFT is the method of choice for speed and ability to capture the variability .. this shold take just a few minutes
-
-# 2 mins
-require(aegis)
 require(aegis.bathymetry)
 
 scale_ram_required_main_process = 1 # GB twostep / fft ---
@@ -25,7 +21,6 @@ p0 = aegis::spatial_parameters( spatial_domain="bathymetry_example",
   dres=1/60/4, pres=0.5, lon0=-64, lon1=-62, lat0=44, lat1=45, psignif=2 )
 
 # or:
-
 # p0 = stmv_test_data( "aegis.test.parameters")
 
 DATA = list(
@@ -39,8 +34,8 @@ DATA$input = DATA$input[ which(is.finite(DATA$input$z)), ]
 
 p = bathymetry_parameters(
   p=p0,  # start with spatial settings of input data
-  project_class="stmv",
-  stmv_model_label="fft_tapered_statsgrid10km",  # this label is used as directory for storage
+  project_class="hybrid",
+  stmv_model_label="carstm_statsgrid10km",  # this label is used as directory for storage
   data_root = file.path(getwd(), "bathymetry_example"),
   DATA = DATA,
   spatial_domain = p0$spatial_domain,
@@ -49,14 +44,9 @@ p = bathymetry_parameters(
   aegis_dimensionality="space",
   stmv_variables = list(Y="z"),  # required as fft has no formulae
   stmv_global_modelengine = "none",  # too much data to use glm as an entry into link space ... use a direct transformation
-  stmv_local_modelengine="fft",
-  stmv_fft_filter = "matern tapered lowpass modelled fast_predictions", #  matern with taper, fast predictions are sufficient as data density is high
-  stmv_lowpass_nu = 0.5,
-  stmv_lowpass_phi = stmv::matern_distance2phi( distance=0.2, nu=0.5, cor=0.1 ),  # note: p$pres = 0.5
-  stmv_variogram_method = "fft",
-  stmv_autocorrelation_fft_taper = 0.7,  # benchmark from which to taper .. high data density truncate local predictions to capture heterogeneity
-  stmv_autocorrelation_localrange = 0.1,
-  stmv_autocorrelation_basis_interpolation = c(0.5, 0.25, 0.1, 0.01),  # start with smaller localrange estimates and expand
+  stmv_local_modelengine="carstm",
+  # stmv_au_distance_reference="completely_inside_boundary",
+  # stmv_au_buffer_links=1,
   stmv_filter_depth_m = FALSE,  # need data above sea level to get coastline
   stmv_Y_transform =list(
     transf = function(x) {log10(x + 2500)} ,
@@ -64,36 +54,19 @@ p = bathymetry_parameters(
   ), # data range is from -1667 to 5467 m: make all positive valued
   stmv_rsquared_threshold = 0.01, # lower threshold  .. i.e., ignore ... there is no timeseries model, nor a fixed effect spatial "model"
   stmv_distance_statsgrid = 10, # resolution (km) of data aggregation (i.e. generation of the ** statistics ** )
-  stmv_distance_scale = c( 2.5, 5, 10, 20, 40, 60, 80 ), # km ... approx guesses of 95% AC range
-  stmv_distance_basis_interpolation = c( 5, 10, 15, 20, 40, 60, 80  ) , # range of permissible predictions km (i.e 1/2 stats grid to upper limit) .. in this case 5, 10, 20
-  stmv_distance_prediction_limits =c( 2.5, 10 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
+  # stmv_distance_scale = c( 2.5, 5, 10, 20, 40, 60, 80 ), # km ... approx guesses of 95% AC range
+  stmv_distance_prediction_limits =c( 5, 10 ), # range of permissible predictions km (i.e 1/2 stats grid to upper limit based upon data density)
   stmv_nmin = 50,  # min number of data points req before attempting to model in a localized space
   stmv_nmax = 600, # no real upper bound.. just speed /RAM
-  stmv_force_complete_method = "fft",
+  stmv_force_complete_method = "linear_interp",
   stmv_runmode = list(
-    scale = rep("localhost", scale_ncpus),
-    interpolate = list(
-      c1 = rep("localhost", interpolate_ncpus),  # ncpus for each runmode
-      c2 = rep("localhost", interpolate_ncpus),  # ncpus for each runmode
-      c3 = rep("localhost", max(1, interpolate_ncpus-1)),
-      c4 = rep("localhost", max(1, interpolate_ncpus-2)),
-      c5 = rep("localhost", max(1, interpolate_ncpus-2))
-    ),
-    # if a good idea of autocorrelation is missing, forcing via explicit distance limits is an option
-    # interpolate_distance_basis = list(
-    #   d1 = rep("localhost", interpolate_ncpus),
-    #   d2 = rep("localhost", interpolate_ncpus),
-    #   d3 = rep("localhost", max(1, interpolate_ncpus-1)),
-    #   d4 = rep("localhost", max(1, interpolate_ncpus-1)),
-    #   d5 = rep("localhost", max(1, interpolate_ncpus-2)),
-    #   d6 = rep("localhost", max(1, interpolate_ncpus-2))
-    # ),
-    interpolate_predictions = TRUE,
+    carstm = rep("localhost", scale_ncpus),
     globalmodel = FALSE,
     restart_load = FALSE,
     save_completed_data = TRUE # just a dummy variable with the correct name
-  )  # ncpus for each runmode
+  )
 )
+
 
 if (0) {
   # to force serial mode
@@ -131,16 +104,13 @@ statsvars = dimnames(statistics)[[2]]
 # statsvars = c("sdTotal", "ndata", "fixed_mean", "fixed_sd", "dic", "dic_p_eff",
 #   "waic", "waic_p_eff", "mlik", "Expected_number_of_parameters",
 #   "Stdev_of_the_number_of_parameters", "Number_of_equivalent_replicates",
-#   "Precision_for_the_Gaussian_observations", "Precision_for_auid",
-#   "Phi_for_auid", "Precision_for_the_Gaussian_observations_sd", "Precision_for_auid_sd", "Phi_for_auid_sd"
+#   "Precision_for_the_Gaussian_observations", "Precision_for_aui",
+#   "Phi_for_aui", "Precision_for_the_Gaussian_observations_sd", "Precision_for_aui_sd", "Phi_for_aui_sd"
 # )
 
 # statsvars = c( "sdTotal", "rsquared", "ndata", "sdSpatial", "sdObs", "phi", "nu", "localrange" )
 dev.new(); levelplot( predictions[] ~ locations[,1] + locations[,2], aspect="iso" )
-dev.new(); levelplot( statistics[,match("nu", statsvars)]  ~ locations[,1] + locations[,2], aspect="iso" ) # nu
+dev.new(); levelplot( statistics[,match("Phi_for_auid", statsvars)]  ~ locations[,1] + locations[,2], aspect="iso" ) # nu
 dev.new(); levelplot( statistics[,match("sdTotal", statsvars)]  ~ locations[,1] + locations[,2], aspect="iso" ) #sd total
-dev.new(); levelplot( statistics[,match("localrange", statsvars)]  ~ locations[,1] + locations[,2], aspect="iso" ) #localrange
-
-
-
+dev.new(); levelplot( statistics[,match("rsquared", statsvars)]  ~ locations[,1] + locations[,2], aspect="iso" ) #localrange
 
